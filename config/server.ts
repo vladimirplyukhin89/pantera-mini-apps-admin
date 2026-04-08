dac5e2ab80afa6d8ebcc4a7f9364f1da6589475b
Vladimir Plyukhin <vladimirplyukhin89@gmail.com>
2026-04-08 14:14:11 +0500

    refactor: remove vercel server

--- config/server.ts
+++ config/server.ts
@@ -3,6 +3,8 @@ import type { Core } from '@strapi/strapi';
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  /** Продакшен: PUBLIC_URL=https://cms.ваш-домен.ru для корректных URL админки и API */
+  url: env('PUBLIC_URL', 'http://localhost:1337'),
   app: {
     keys: env.array('APP_KEYS'),
   },
